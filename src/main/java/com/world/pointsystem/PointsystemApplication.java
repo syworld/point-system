f746bb79ca047b3fa9df340f719e38747df1781d
syworld <syworldy@gmail.com>
2022-07-10 21:24:59 +0900

    Initial commit

---
+++ src/main/java/com/world/pointsystem/PointsystemApplication.java
@@ -0,0 +1,13 @@
+package com.world.pointsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PointsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/world/pointsystem/PointsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.world.pointsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
