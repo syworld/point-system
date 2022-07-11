aac952f1e2b33ccd281a69510a2f31c8cd2a3176
syworld <syworldy@gmail.com>
2022-07-11 23:36:19 +0900

    feat: MyBatis ReviewEventMapper 구현

---
+++ src/main/java/com/world/pointsystem/event/dao/ReviewEventMapper.java
@@ -0,0 +1,17 @@
+package com.world.pointsystem.event.dao;
+
+import com.world.pointsystem.event.entity.ReviewEvent;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ReviewEventMapper {
+
+  void insert(ReviewEvent reviewEvent);
+
+  ReviewEvent findRecentByUserIdAndPlaceId(String placeId, String userId);
+
+  ReviewEvent findByPlaceIdAndFirstPoint(String placeId);
+
+  Integer countPointByUserId(String userId);
+
+}
