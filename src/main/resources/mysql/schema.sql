c9dbdb631232ea5c832fb51a29ddb3b4193447ba
syworld <syworldy@gmail.com>
2022-07-13 01:35:39 +0900

    feat: review_event schema index 수정

--- src/main/resources/mysql/schema.sql
+++ src/main/resources/mysql/schema.sql
@@ -1,6 +1,6 @@
 create table review_event
 (
-    id             BIGINT                              NOT NULL AUTO_INCREMENT
+    id            bigint auto_increment
         primary key,
     user_id       varchar(255)                        not null,
     review_id     varchar(255)                        not null,
@@ -8,16 +8,16 @@ create table review_event
     content_point int                                 not null,
     image_point   int                                 not null,
     first_point   int                                 not null,
-    action        enum ('ADD', 'MOD', 'DELETE')   not null,
+    action        enum ('ADD', 'MOD', 'DELETE')       not null,
     created_at    timestamp default CURRENT_TIMESTAMP not null
 );
 
-create index IDX_place_first_point
-    on review_event (place_id, first_point);
+create index IDX_created
+    on review_event (created_at);
+
+create index IDX_place_user
+    on review_event (place_id, user_id);
 
 create index IDX_user
     on review_event (user_id);
-
-create index IDX_created
-    on review_event (created_at);
 
